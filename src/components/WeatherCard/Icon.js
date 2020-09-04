2af23182388ce2ad4f34496d1fc3c33d8d77cdbf
Zayan Asad <zayan.asad@sky.uk>
2020-09-04 17:05:36 +0100

    changed img pathg

--- src/components/WeatherCard/Icon.js
+++ src/components/WeatherCard/Icon.js
@@ -9,34 +9,34 @@ const Icon = ({ condition }) => {
   var icon = "";
   switch (condition) {
     case "Clear":
-      icon = `./img/Mostly Sunny-2x.png`;
+      icon = `/img/Mostly Sunny-2x.png`;
       break;
     case "Clouds":
-      icon = `./img/Mostly Cloudy-2x.png`;
+      icon = `/img/Mostly Cloudy-2x.png`;
       break;
     case "Haze":
-      icon = `./img/Haze-2x.png`;
+      icon = `/img/Haze-2x.png`;
       break;
     case "Fog":
-      icon = `./img/Fog-2x.png`;
+      icon = `/img/Fog-2x.png`;
       break;
     case "Tornado":
-      icon = `./img/Tornado-2x.png`;
+      icon = `/img/Tornado-2x.png`;
       break;
     case "Dust":
-      icon = `./img/Dust-2x.png`;
+      icon = `/img/Dust-2x.png`;
       break;
     case "Mist":
-      icon = `./img/Snow-2x.png`;
+      icon = `/img/Snow-2x.png`;
       break;
     case "Rain":
-      icon = `./img/Rain-2x.png`;
+      icon = `/img/Rain-2x.png`;
       break;
     case "Drizzle":
-      icon = `./img/Drizzle-2x.png`;
+      icon = `/img/Drizzle-2x.png`;
       break;
     case "Thunderstorm":
-      icon = `./img/Severe Thunderstorm-2x.png`;
+      icon = `/img/Severe Thunderstorm-2x.png`;
       break;
     default:
       icon = "No image found";
